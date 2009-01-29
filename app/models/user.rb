651e06c2a2cce6764093ab86088fd2ee854f7f3e
dani <danigb@gmail.com>
2009-01-29 12:21:40 +0100

    mysql ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :groups
   has_many :pendings
-  has_many :clips, :through => :pendings, :order => '"clips".id desc'
+  has_many :clips, :through => :pendings, :order => 'id desc'
 
   COLORS = [['verde', '#58DF37'], ['violeta', '#923BEF'], ['gris', '#CFCFCF'], ['cyan', '#86EFCF'], ['amarillo', '#FFE900']]
 
